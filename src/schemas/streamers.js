ba9a55bffa30dffd8bdcebf918fc19e7b78802e8
fnalte <112013608+fnalte123@users.noreply.github.com>
2023-01-18 00:55:00 +0100

    Delete src directory

--- src/schemas/streamers.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-const streamerSchema = new Schema({
-  _id: Schema.Types.ObjectId,
-  guildId: String,
-  streamername: String,
-  IsLive: false,
-});
-
-module.exports = model("Streamers", streamerSchema, "streamers");
--- src/schemas/ticket.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-const ticketSchema = new Schema({
-  _id: Schema.Types.ObjectId,
-  guildId: String,
-  discordid: String,
-  ticketnummer: String,
-});
-
-module.exports = model("Ticket", ticketSchema, "tickets");
